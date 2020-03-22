970bf71fa5980ad809063d173ab512274b9a859d
Shailendra Dobhal <shailendra.dobhal@walmart.com>
2020-03-22 20:58:15 +0530

    Updates

---
+++ precache-manifest.b9b127db55668f9f5c170d83ee271c5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f37462f3bbe1e83b4736b10c9ce3a8a",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "d8c906500ddda3950034",
+    "url": "/starwars/static/css/main.9d2ef184.chunk.css"
+  },
+  {
+    "revision": "fb42efa68f9124a6c673",
+    "url": "/starwars/static/js/2.58530b50.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/starwars/static/js/2.58530b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c906500ddda3950034",
+    "url": "/starwars/static/js/main.79ac5a43.chunk.js"
+  },
+  {
+    "revision": "97ea1d26b8111e1d2ea8",
+    "url": "/starwars/static/js/runtime-main.b6d8afef.js"
+  }
+]);
